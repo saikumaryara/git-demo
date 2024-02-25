3b99f942a139849af565e6e92c6792961ba5812e
saikumaryara <61284081+saikumaryara@users.noreply.github.com>
2024-02-25 09:31:16 +0530

    Update GitTest.java

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,9 +5,12 @@ public class GitTest {
       
 	 System.out.println("Added new line");
 	}
-
+        
 	public void add() {
 		System.out.println("new method added");
 	}
 
+	public void sub() {
+		System.out.println("new sub added");
+	}
 }
